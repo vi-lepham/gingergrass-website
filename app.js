f41a54cc21a50ac80e71c94f92233e579364c158
vilepham <vilepham136@gmail.com>
2020-07-03 20:08:53 +0900

    Enable opening section, moved reservation section to replace the info section, and changed stylings

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
+// Opening
+TweenMax.staggerFrom(".o-logo div", 1.5, {
+    opacity: 0,
+    y: 50,
+    ease: Expo.easeInOut
+}, 0.2)
+TweenMax.to(".o-image", 1.5, {
+    height: '60vh',
+    delay: 1.6,
+    ease: Expo.easeInOut
+})
+TweenMax.to(".o-image img", 1.5, {
+    height: '100%',
+    delay: 2.3,
+    ease: Expo.easeInOut
+})
+TweenMax.to(".s-opening", 2, {
+    y: '-100vh',
+    height: 0,
+    delay: 2.7,
+    ease: Expo.easeInOut
+})
+TweenMax.to("main", 2, {
+    overflow: 'visible',
+    height: '100%',
+    delay: 3,
+    ease: Expo.easeInOut
+})
+
 // UI Class
 class UI {
-    static toggleReserve() {
-        const logo = document.querySelector('.h-logo');
-        const reserve = document.querySelector('.reserve-content');
-        reserve.classList.add('active');
-        setTimeout(() => {
-            logo.classList.add('hide');
-        }, 500); 
-    }
-    static closeReserve() {
-        const logo = document.querySelector('.h-logo');
-        const reserve = document.querySelector('.reserve-content');
-        reserve.classList.remove('active');
-        setTimeout(() => {
-            logo.classList.remove('hide');
-        }, 500); 
-    }
     static toggleBurger() {
         const burger = document.querySelector('.burger-menu-content');
         const reserveBtn = document.querySelector('.reserve-btn');
@@ -40,16 +53,19 @@ class UI {
     }
 }
 
-// Event Listeners for reservation window 
-document.querySelectorAll('.reserve-btn').forEach(button => {
-    button.addEventListener('click', UI.toggleReserve);
-})
-document.querySelector('.close-btn').addEventListener('click', UI.closeReserve);
-
 // Event Listener for burger menu window
 document.querySelector('.burger-btn').addEventListener('click', () => {
     UI.toggleBurger();
     UI.animateBurgerIcon();
+})
+
+// Event Listener for click in any burger menu options
+const burgerOptions = document.querySelectorAll('.burger-menu-content ul li');
+burgerOptions.forEach(option => {
+    option.addEventListener('click', () => {
+        UI.toggleBurger();
+        UI.animateBurgerIcon();
+    })
 })
 
 // Form Class
@@ -115,72 +131,8 @@ const observerNav = new IntersectionObserver(function(entries, observer) {
             reserveBtn.classList.remove('hide');
             logo.classList.remove('hide');
         }
-        console.log(reserveBtn.classList.contains('hide'));
     })
 }, {threshold: 1, rootMargin: "0px 200px 0px 0px"})
 
 observerNav.observe(hero);
-
-// Autoplay Slide
-const carousel = document.querySelector('.c-track');
-const carouselImg = document.querySelectorAll('.c-track li');
-const nextBtn = document.querySelector('.right-button');
-const prevBtn = document.querySelector('.left-button');
-let index = 1;
-const size = carouselImg[0].clientWidth;
-
-class Slide {
-    static infiniteSlide() {
-        
-        carousel.addEventListener('transitionend', () => {
-            if (carouselImg[index].id === 'lastClone') {
-                carousel.style.transition = 'none';
-                index = carouselImg.length - 2;
-                carousel.style.transform = 'translateX(' + (-size *index) + 'px)';
-            }
-            if (carouselImg[index].id === 'firstClone') {
-                carousel.style.transition = 'none';
-                index = carouselImg.length - index;
-                carousel.style.transform = 'translateX(' + (-size *index) + 'px)';
-            }
-        })
-    }
-    static nextSlide() {
-        index++;
-        carousel.style.transform = 'translateX(' + (-size *index) + 'px)';
-        carousel.style.transition = 'transform 1s ease-in-out';
-    }
-    static prevSlide() {
-        index--;
-        carousel.style.transform = 'translateX(' + (-size *index) + 'px)';
-        carousel.style.transition = 'transform 1s ease-in-out';
-    }
-}
-
-Slide.infiniteSlide();
-nextBtn.addEventListener('click', Slide.nextSlide);
-prevBtn.addEventListener('click', Slide.prevSlide);
-
-
-//Toggle Menu on each List Button Click
-const menuLists = document.querySelectorAll('.menu-list li');
-
-menuLists.forEach(list => {
-    list.addEventListener('click', () => {
-        // const toggledMenu = document.querySelectorAll('.toggle-menu li');
-        const active = document.querySelectorAll('.active');
-        // const displayed = document.querySelectorAll('.displayed');
-
-        if (active.length > 0) {
-            active[0].classList.remove('active');
-        }
-        list.className += 'active';
-        
-        // if (displayed.length > 0) {
-        //     displayed[0].classList.remove('displayed');
-        // }
-        // toggledMenu[index].className += 'displayed';
-    });
-})
-
 
