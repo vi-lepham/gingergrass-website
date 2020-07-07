76805282f85b2e47b67aca49718a1145ef503af8
vilepham <vilepham136@gmail.com>
2020-07-07 20:17:26 +0900

    Added TweenMax back

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 // Opening
 
-// TweenMax.staggerFrom(".o-logo div", 1.5, {
-//     opacity: 0,
-//     y: 50,
-//     ease: Expo.easeInOut
-// }, 0.2)
+TweenMax.staggerFrom(".o-logo div", 1.5, {
+    opacity: 0,
+    y: 50,
+    ease: Expo.easeInOut
+}, 0.2)
 
-// TweenMax.from(".o-image", 1.5, {
-//     height: 0,
-//     delay: 1.6,
-//     ease: Expo.easeInOut
-// })
+TweenMax.from(".o-image", 1.5, {
+    height: 0,
+    delay: 1.6,
+    ease: Expo.easeInOut
+})
 
-// TweenMax.from(".s-opening", 2, {
-//     opacity: 1,
-//     height: "100vh",
-//     delay: 2.7,
-//     ease: Expo.easeInOut
-// })
+TweenMax.from(".s-opening", 2, {
+    opacity: 1,
+    height: "100vh",
+    delay: 2.7,
+    ease: Expo.easeInOut
+})
 
-// TweenMax.from(".main", 2, {
-//     overflow: "hidden",
-//     height: 0,
-//     delay: 3,
-//     ease: Expo.easeInOut
-// })
+TweenMax.from(".main", 2, {
+    overflow: "hidden",
+    height: 0,
+    delay: 3,
+    ease: Expo.easeInOut
+})
 
 // UI Class
 class UI {
