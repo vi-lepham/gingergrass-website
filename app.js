62b55b6da760fdfdbd27b0e045e662577db899ab
vilepham <vilepham136@gmail.com>
2020-07-19 16:09:31 +0900

    Fixed lazy load intersection observer

--- app.js
+++ app.js
@@ -186,11 +186,13 @@ observerNav.observe(hero);
 const lazyLoad = document.querySelectorAll('.lazy-load');
 const observerLazyLoad = new IntersectionObserver(function(entries, observer) {
     entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            lazyLoad.classList.remove('lazy-load'); 
+        if (!entry.isIntersecting) {
+            return;
         } 
+        entry.target.classList.add('appear');
+        observer.unobserve(entry.target);
     })
-}, {threshold: 1, rootMargin: "0px 200px 0px 0px"})
+}, {threshold: 0.2})
 
 lazyLoad.forEach(item => observerLazyLoad.observe(item))
 
